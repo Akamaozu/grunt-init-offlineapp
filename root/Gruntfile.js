94954c2afd927d2e8632a37144e0aa6fa31b7479
Uzo Olisemeka <uzo@designbymobius.ca>
2014-02-10 08:46:04 +0100

    Basic Task Configurations

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -1,14 +1,128 @@
 module.exports = function(grunt){
 	
-	// Configure Plugins
 		grunt.initConfig({
 
-			pkg: grunt.file.readJSON('package.json'),
+			// req vars
+				pkg: grunt.file.readJSON('package.json'),
 
-			srcDirectory: "{%= src_dir_name %}",
-			debugDirectory: "{%= debug_dir_name %}",
-			buildDirectory: "{%= concat_dir_name %}",
-			prodDirectory: "{%= prod_dir_name %}"
+				srcDirectory: "{%= src_dir_name %}",
+				debugDirectory: "{%= debug_dir_name %}",
+				buildDirectory: "{%= concat_dir_name %}",
+				prodDirectory: "{%= prod_dir_name %}",
+
+			// array of assets			
+				coreJS: [],
+				coreCSS: [],
+
+		// Configure Plugins
+
+			// jshint
+				jshint: {
+					options: {
+
+						devel: true,
+						browser: true,
+						loopfunc: true,
+						lastsemic: true
+					},
+
+					beforeconcat: '<%= coreJS %>'
+				},
+
+			// uglify
+				uglify: {
+			
+					core_scripts: {
+						files: {
+							'<%= srcDirectory %>/js/min/core.min.js': '<%= coreJS %>'
+						}
+					}
+				},
+
+			// cssmin
+				cssmin: {
+					
+					minify: {
+						src: '<%= coreCSS %>',
+						dest: '<%= srcDirectory %>/css/min/base.min.css'
+					}
+				},
+
+			// htmlbuild
+				htmlbuild: {
+
+			        debug: {
+			            src: '<%= srcDirectory %>/index.*', 
+			            dest: '<%= debugDirectory %>/index.*',
+			            options: {
+			                scripts: {
+			                    core: '<%= coreJS %>',
+			                },
+			                styles: {
+			                    base: '<%= coreCSS %>',
+			                }
+			            }
+			        },
+
+			        production: {
+			            src: '<%= srcDirectory %>/index.*', 
+			            dest: '<%= buildDirectory %>/index.*',
+			            options: {
+			                
+			                scripts: {
+			                    core: '<%= srcDirectory %>/js/min/core.min.js',
+			                },
+			                
+			                styles: {
+			                    base: '<%= srcDirectory %>/css/min/base.min.css',
+			                },
+			                
+			                collapseWhitespace: true
+			            }
+			        }
+			    },
+
+		// manifest
+			manifest: {
+
+				generate: {
+					
+					src: ['index.*'],
+					dest: '<%= buildDirectory %>/manifest.appcache',
+					options: {
+						basePath:"<%= buildDirectory %>/",
+						fallback: ["/ index.html"],
+						network: ["*"],	
+						preferOnline: true,
+				        timestamp: true,
+				        verbose: false,
+					},				
+				}
+			},
+
+		// watch
+			watch: {
+
+				js: {
+					
+					files: '<%= coreJS %>',
+					tasks: ['jshint:beforeconcat', 'htmlbuild:debug', 'manifest', 'uglify', 'jshint:afterconcat', 'htmlbuild:production', 'copy:manifest' ]
+				},
+
+				css: {
+
+					files: '<%= coreCSS %>',
+					tasks: ['cssmin', 'htmlbuild', 'manifest', 'copy:manifest']
+				},
+
+				index: {
+
+					files: '<%= srcDirectory %>/index.*',
+					tasks: ['htmlbuild', 'manifest', 'copy:manifest']
+				}
+
+	        },
+
 		});
 
 	// Load Plugins	
